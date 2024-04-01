f8ab7ea8f54f546ec566202792fa41e2c161b885
Graham Holtslander <gholtslander@digitalocean.com>
2024-03-31 18:57:14 -0600

    feat: add headings to SavedSearches

--- search/src/SavedSearches.jsx
+++ search/src/SavedSearches.jsx
@@ -44,34 +44,41 @@ export function SavedSearches({ visible, searches, setSearches }) {
 
   return (
     <Collapse in={visible}>
-      <div id="searches">
-        <div className="row">
-          {searches.map((search) => {
-            return (
-              <SavedSearch
-                key={search.id}
-                search={search}
-                handleDelete={handleDelete}
-                handleEdit={handleEdit}
-              />
-            );
-          })}
+      <>
+        <div className="ms-4 row">
+          <div className="col-2">Actions</div>
+          <div className="col-3">Name</div>
+          <div className="col-7">Query</div>
         </div>
-        <div className="row my-2">
-          <div className="col-12 text-muted text-center">
-            <span style={{ fontSize: ".9625rem" }}>
-              This history includes most recent or saved searches. Use the
-              Actions icons to save/name searches and preserve up to 10 in this
-              list.
-            </span>
+        <div id="searches">
+          <div className="row">
+            {searches.map((search) => {
+              return (
+                <SavedSearch
+                  key={search.id}
+                  search={search}
+                  handleDelete={handleDelete}
+                  handleEdit={handleEdit}
+                />
+              );
+            })}
           </div>
-          <div className="col-4 offset-md-4 text-center">
-            <Button variant="secondary" onClick={handleDeleteAll}>
-              Delete all previous searches
-            </Button>
+          <div className="row my-2">
+            <div className="col-12 text-muted text-center">
+              <span style={{ fontSize: ".9625rem" }}>
+                This history includes most recent or saved searches. Use the
+                Actions icons to save/name searches and preserve up to 10 in
+                this list.
+              </span>
+            </div>
+            <div className="col-4 offset-md-4 text-center">
+              <Button variant="secondary" onClick={handleDeleteAll}>
+                Delete all previous searches
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
+      </>
     </Collapse>
   );
 }
